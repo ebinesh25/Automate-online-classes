c5682d5fea52c8ac1eeadba052d31cd85ed0367d
Ebi <ebinesh2511@gmail.com>
2023-07-24 13:08:27 +0530

    Modified to open Gmeet
    Modified the code to open a meet link and mute all the buttons and join and put GOood morning mesage in the chat box

--- twt.py
+++ twt.py
@@ -1,26 +1,46 @@
-
-# I edited this in the branch name Implemetation using selenimum
-# Hey Now I am editing this from the main branch see whther you can pull this down to you local machine
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 PATH = "/home/positron/Downloads/chromedriver-linux64"
 driver = webdriver.Chrome(PATH)
-driver.get("https://techwithtim.net")
+actions = ActionChains(driver)
+
+
+driver.get("https://meet.google.com/dve-fjud-dth")
 print(driver.title)
-search = driver.find_element_by_name("s")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
+
+# class name for mute button is class="GKGgdd"
+# For video mute clas="GOH7Zb"
+
+
+# search = driver.find_element_by_name("s")
+# search.send_keys("test")
+# search.send_keys(Keys.RETURN)
+
+actions.key_down(Keys.CONTROL).send_keys("d").key_up(Keys.CONTROL) # To mute mic
+actions.key_down(Keys.CONTROL).send_keys("e").key_up(Keys.CONTROL) # To mute camera
+
+join = driver.find_element_by_xpath("//*[text()='Join now']")
+join.click()
+
 try:
-     main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main")))
+     main = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ow3")))
+
+     list_of_buttons = main.find_elements_by_class("r6xAKc")
+
+     chat = list_of_buttons[3]
+     chat.click()
+
+     textBox = main.find_elements_by_class("NiQxf")
+     textBox.send_keys("Good Morning Makka")
      
-     articles = main.find_elements_by_tag_name("article")
-     for article in articles:
-        header = article.find_element_by_class_name("entry-sumary")
-        print (header.text)
+    #  articles = main.find_elements_by_tag_name("article")
+    #  for article in articles:
+    #     header = article.find_element_by_class_name("entry-sumary")
+    #     print (header.text) 
 finally:
     driver.quit()
